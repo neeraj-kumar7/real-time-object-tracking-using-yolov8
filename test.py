502f3707d854f784a27cb266ddd1fbc43811fa23
neeraj <neerajpenumaka7@gmail.com>
2023-02-16 10:00:58 +0530

    tracking using webcam

---
+++ test.py
@@ -0,0 +1,6 @@
+from ultralytics import YOLO
+from ultralytics.yolo.v8.detect.predict import DetectionPredictor
+import cv2
+
+model = YOLO("yolov8s.pt")
+results = model.predict(source="0", show=True)
